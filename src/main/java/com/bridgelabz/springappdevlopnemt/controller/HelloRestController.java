5eb6f7617725ebc673b2a1fd2b288fea1ef6a0e2
PatilBhavesh84 <107094696+PatilBhavesh84@users.noreply.github.com>
2022-08-25 22:37:17 +0530

    Update HelloRestController.java

--- src/main/java/com/bridgelabz/springappdevlopnemt/controller/HelloRestController.java
+++ src/main/java/com/bridgelabz/springappdevlopnemt/controller/HelloRestController.java
@@ -5,17 +5,17 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/hello")
 public class HelloRestController {
-
+    //localhost:8080/hello/home
     @RequestMapping(value = {"", "/", "/home"})
     public String sayHello() {
         return "Hello From BridgeLabz";
     }
-
+    //localhost:8080/hello/query?name=Bhavesh
     @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
     public String sayHello(@RequestParam(value = "name") String name) {
         return "Hello " + name + "!";
     }
-
+    //localhost:8080/hello/param/Bhavesh
     @GetMapping("/param/{name}")
     public String sayHelloParam(@PathVariable String name) {
         return "Hello " + name;
