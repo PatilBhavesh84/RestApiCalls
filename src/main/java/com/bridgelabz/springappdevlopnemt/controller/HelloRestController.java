909b19d9fb924023d604b03e52803853807cdbcc
PatilBhavesh84 <bhaveshpatil8448@gmail.com>
2022-08-25 13:33:15 +0530

    Added:Use GET Request Method and pass name as

--- src/main/java/com/bridgelabz/springappdevlopnemt/controller/HelloRestController.java
+++ src/main/java/com/bridgelabz/springappdevlopnemt/controller/HelloRestController.java
@@ -1,13 +1,11 @@
 package com.bridgelabz.springappdevlopnemt.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/hello")
 public class HelloRestController {
+
     @RequestMapping(value = {"", "/", "/home"})
     public String sayHello() {
         return "Hello From BridgeLabz";
@@ -16,5 +14,10 @@ public class HelloRestController {
     @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
     public String sayHello(@RequestParam(value = "name") String name) {
         return "Hello " + name + "!";
+    }
+
+    @GetMapping("/param/{name}")
+    public String sayHelloParam(@PathVariable String name) {
+        return "Hello " + name;
     }
 }
