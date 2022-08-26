4f876d276b92bb981d86f105058b5956f2f552da
PatilBhavesh84 <bhaveshpatil8448@gmail.com>
2022-08-26 11:24:19 +0530

    AddedUse POST Request Method and pass first name and:

--- src/main/java/com/bridgelabz/springappdevlopnemt/SpringAppDevlopnemtApplication.java
+++ src/main/java/com/bridgelabz/springappdevlopnemt/SpringAppDevlopnemtApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringAppDevlopnemtApplication {
 
     public static void main(String[] args) {
+        System.out.println("Welcome to messaging application demonstrates REST API calls ");
         SpringApplication.run(SpringAppDevlopnemtApplication.class, args);
     }
 
--- src/main/java/com/bridgelabz/springappdevlopnemt/controller/HelloRestController.java
+++ src/main/java/com/bridgelabz/springappdevlopnemt/controller/HelloRestController.java
@@ -1,23 +1,32 @@
 package com.bridgelabz.springappdevlopnemt.controller;
 
+import com.bridgelabz.springappdevlopnemt.Model.User;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/hello")
 public class HelloRestController {
-
+    //localhost:8080/hello/home
     @RequestMapping(value = {"", "/", "/home"})
     public String sayHello() {
         return "Hello From BridgeLabz";
     }
 
+    //localhost:8080/hello/query?name=Bhavesh
     @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
     public String sayHello(@RequestParam(value = "name") String name) {
         return "Hello " + name + "!";
     }
 
+    //localhost:8080/hello/param/bhavesh
     @GetMapping("/param/{name}")
     public String sayHelloParam(@PathVariable String name) {
+
         return "Hello " + name;
+    }
+    //localhost:8080/hello/post
+    @PostMapping("/post")
+    public String sayHello(@RequestBody User user) {
+        return "Hello " + user.getFirstName() + " " + user.getLastName() + " !";
     }
 }
